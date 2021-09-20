7fe4d977148997124cd37f0b26d4c2b4bc896f99
jay prakash <bjayaprakash7345@gmail.com>
2021-09-20 14:38:02 +0530

    first commit

---
+++ Controllers/MenuItemsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeCheckApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemsController : ControllerBase
+    {
+        List<MenuItem> items = new List<MenuItem>()
+            {
+                new MenuItem() { Id=1,Name="Chicken 65",FreeDelivery=true,Price=150.00,DateOfLaunch=DateTime.Now,Active=true},
+                new MenuItem() { Id=2,Name="Chicken lollypop",FreeDelivery=true,Price=160.00,DateOfLaunch=DateTime.Now,Active=true},
+                new MenuItem() { Id=3,Name="Chicken Tikka",FreeDelivery=true,Price=250.00,DateOfLaunch=DateTime.Now,Active=false},
+                new MenuItem() { Id=4,Name="Biryani",FreeDelivery=true,Price=250.00,DateOfLaunch=DateTime.Now,Active=true},
+            };
+        [HttpGet]
+        public IActionResult GetItem()
+        {
+            return Ok(items);
+        }
+        [HttpGet("GetById/{id}")]
+        public IActionResult GetById(int id)
+        {
+            MenuItem item = items.Find(s=> s.Id==id);
+            if (item == null)
+                return NotFound("Not found!!!");
+            else
+            {
+                return Ok(item);
+            }
+        }
+    }
+}
